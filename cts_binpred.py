4e6012971096e4a74f4f5993fe279503248b2467
Ran Cui <cranmail@gmail.com>
2019-07-19 09:17:21 -0400

    started writing new script for binned prediction with cell type specific annotations

--- cts_binpred.py
+++ cts_binpred.py
@@ -1,15 +1,64 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import numpy as np
 import argparse
 import pdb
 
-def binPred(cts_result_dir,ldcts_dir,annot_prefix,pip_obj,chrom,result_dir):
-    '''
-    cts_result_dir is the directory that contains all the cell_type_results files for all relevant datasets.
-    ldcts_dir is the directory that contains all relevant ldcts files.
-    annot_prefix is the prefix of some version of baseline annotations.
-    pip_obj is a PIP object that specifies the pip file, pip column name, SNP column name etc.
-    chrom is the chromosome we want to exclude.
-    result_dir is the directory we want to store the results in.
-    '''
-    cts_annotdf = getCtsAnnots(cts_result_dir,ldcts_dir)
+def binPredCV(args):
+    feature = Feature(ref_annot=Prefix('ref_annot','in'),
+                      cts_annot=Prefix('cts_annot','in'),
+                      output_prefix=Prefix('feature','out'))
+    target = Target(target_file=args.target_file,
+                    target_colname=args.target_colname)
+    predchrom = args.chrom
+    binchroms = [x for x in range(1,23) if x != int(predchrom)]
+    binpred =BinPred(predchrom=predchrom,
+                     binchroms=binchroms,
+                     num_bins=args.num_bins,
+                     bin_method=args.bin_method
+                     output_prefix=Prefix('binpred','out'))
+    for binchrom in binchroms:
+        model = Model(method=args.method,
+                      exclude_chrs=[predchrom,binchrom],
+                      confounders=args.confounder_names,
+                      output_prefix=Prefix('model','out'))
+        model.fit(feature,target)
+        ypred_binchrom = model.predict(feature.binchrom.withpip,remove_confounders=True)
+        ypred_predchrom = model.predict(feature.predchrom.all,remove_confounders=True)
+        binpred.compute_binpred(ypred_binchrom,ypred_predchrom)
+    binpred.compute_meanbinpred()
+    return
+
+
+def Prefix(name,in_out):
+    if name=='feature' and in_out=='out':
+        return args.feature_output_prefix
+
+
+class Feature:
+
+    def __init__(self,ref_annot,cts_annot):
+        self.ref_annot = ref_annot # reference annotation prefix
+        self.cts_annot = cts_annot # cell type specific annotation prefix
+
+
+class Target:
+
+    def __init__(self,target_file,target_colname='prob'):
+        self.target_file = target_file # usually PIP file with all fine-mapped SNPs
+        self.target_colname = target_colname # usually the column name for PIP
+
+
+class BinPred:
+
+    def __init__(self,predchrom,binchroms,num_bins=5,bin_method='equally-sized'):
+        self.predchrom = predchrom # integer
+        self.binchroms = binchroms # list (can be list of one element)
+        self.num_bins = num_bins
+        self.bin_method = bin_method # equally-sized or equally-spaced
+
+
+class Model:
+
+    def __init__(self,method='OLS',exclude_chrs,confounders,output_prefix)
